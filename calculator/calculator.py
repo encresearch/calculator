ed7db0b0088b32ad918206e0c4f9bdc6c0785e97
Seb <seb.arboleda@outlook.com>
2020-07-06 09:35:30 -0400

    Cleanup code and added linting files.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,28 +1,31 @@
-import os, time
+import time
+import json
+from datetime import datetime
+
 import paho.mqtt.client as mqtt
 import pandas as pd
-import numpy as np
-from datetime import datetime
-from influxAPI import InfluxConnection
-from conversionFunctions import sensor_functions
-import json
 
-#Declaring Global Variables
-HOST = 'iot.eclipse.org'#"10.128.189.236"
-PORT = 1883
-KEEPALIVE = 120
+from calculator.influxAPI import InfluxConnection
+from calculator.conversionFunctions import sensor_functions
 
-#MQTT Topic to communicate with Connector
-topic =  "communication/influxdbUpdate"
-
-topic_inspector = "communication/influxdbConverted"
-#MQTT Client ID to remain unique
-client_id = "/Calculator"
+BROKER_HOST = 'mqtt.eclipse.org'
+BROKER_PORT = 1883
+BROKER_KEEPALIVE = 120
+MQTT_CLIENT_ID = "/Calculator"
+# MQTT Topic to communicate with Connector and Inpsector
+CONNECTOR_MQTT_TOPIC = "communication/influxdbUpdate"
+INPECTOR_MQTT_TOPIC = "communication/influxdbConverted"
 
 
-def connect_to_broker(client_id, host, port, keepalive, on_connect, on_message):
-    ''' Params -> Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
-    We set clean_session False, so in case connection is lost, it'll reconnect with same ID '''
+def connect_to_broker(
+    client_id,
+    host,
+    port,
+    keepalive,
+    on_connect,
+    on_message
+):
+    """Returns an mqtt.Client and mqtt.Client.connection object."""
     client = mqtt.Client(client_id=client_id, clean_session=False)
     client.on_connect = on_connect
     client.on_message = on_message
@@ -31,91 +34,117 @@ def connect_to_broker(client_id, host, port, keepalive, on_connect, on_message):
 
 
 def main():
-
-    #Connection to InfluxDB
+    """The main event."""
     influxdb = InfluxConnection(db_host='localhost')
 
-    #On Connect Function for MQTT Broker
     def on_connect(client, userdata, flags, rc):
-        if rc==0:
+        """On Connect Function for MQTT Broker."""
+        if rc == 0:
             print(f"Connected to MQTT Broker | {datetime.now()}")
-            client.subscribe(topic, 2)
+            client.subscribe(CONNECTOR_MQTT_TOPIC, 2)
         else:
             print(f"Bad Connection Returned (Code: {rc})")
         pass
 
-    #On Message function for MQTT Broker
     def on_message(client, userdata, message):
-        # Function for clients's specific callback when pubslishing message
+        """On Message function for MQTT Broker."""
         initialTime = time.time()
         payload = message.payload.decode()
         print("----------Message Received----------")
         print(datetime.now())
 
-
-
         location, newDataIndexes = json.loads(payload)
 
-        converted_data = [] #Array to store converted Data that will all be pushed to influxDB after all conversions
-        inspectorPayload = [] #Array that will store index information on the new data added to send to inspector
+        # Array to store converted Data that will all be pushed to influxDB
+        # after all conversions.
+        converted_data = []
+        # Array that will store index information on the new data added to send
+        # to inspector.
+        inspectorPayload = []
 
         for adc_channel_pair in newDataIndexes:
-
-            #Unpacking Dictionary
+            # Unpacking Dictionary
             adc = adc_channel_pair['adc']
             channel = adc_channel_pair['channel']
             newDataAmount = adc_channel_pair['amountOfData']
 
-            #Running Query
-            results = influxdb.get_n_recent_readings_with_condition(newDataAmount, adc, channel)
+            # Running Query
+            results = influxdb.get_n_recent_readings_with_condition(
+                newDataAmount,
+                adc,
+                channel
+            )
 
-
-            #Parsing to actual data from results
+            # Parsing to actual data from results
             data = results[0]['values']
 
-            #Getting name of sensor to add to array that will be sent to inspector
-
+            # Getting name of sensor to add to array that will be sent
             sensorNames, _, _, = sensor_functions[adc][channel](data[0][1])
 
-
-            #print(f"Query for adc={adc} and channel={channel} Done: {str(sensorNames)} | {time.time()-initialTime}")
-            #Building Array that will be sent to inspector
+            # Building Array that will be sent to inspector
             if len(sensorNames) == 1:
-                inspectorPayload.append(dict(sensorName=sensorNames[0], adc=adc, channel=channel, amountOfNewData=newDataAmount))
+                inspectorPayload.append(dict(
+                    sensorName=sensorNames[0],
+                    adc=adc,
+                    channel=channel,
+                    amountOfNewData=newDataAmount
+                ))
             else:
                 for sensor in sensorNames:
-                    inspectorPayload.append(dict(sensorName=sensor, adc=adc, channel=channel, amountOfNewData=newDataAmount))
+                    inspectorPayload.append(dict(
+                        sensorName=sensor,
+                        adc=adc,
+                        channel=channel,
+                        amountOfNewData=newDataAmount
+                    ))
 
-            #Converting data points for current ADC and Channel
+            # Converting data points for current ADC and Channel
             for i in range(newDataAmount):
                 rawVoltageValue = data[i][1]
                 timestamp = data[i][0]
 
-                sensorName, units, converted = sensor_functions[adc][channel](rawVoltageValue)
+                sensorName, units, converted = sensor_functions[adc][channel](
+                    rawVoltageValue
+                )
 
-                #Handling Special Case where one sensor reading becomes multiple data points (i.e. MQ9 Sensor records 1 voltage --> 3 Gas Readings)
+                # Handling Special Case where one sensor reading becomes
+                # multiple data points
+                # (i.e. MQ9 Sensor records 1 voltage --> 3 Gas Readings)
                 lengthOfConverted = len(converted)
                 if lengthOfConverted == 1:
-                    newData =  converted[0]
-                    converted_data.append(dict(adc=adc, channel=channel, sensorName=sensorName[0], units=units[0], timestamp=timestamp, data=newData))
+                    newData = converted[0]
+                    converted_data.append(dict(
+                        adc=adc,
+                        channel=channel,
+                        sensorName=sensorName[0],
+                        units=units[0],
+                        timestamp=timestamp,
+                        data=newData
+                    ))
                 else:
                     for j in range(lengthOfConverted):
                         newData = converted[j]
-                        converted_data.append(dict(dict(adc=adc, channel=channel, sensorName=sensorName[j],  units=units[j], timestamp=timestamp, data=newData)))
+                        converted_data.append(dict(dict(
+                            adc=adc,
+                            channel=channel,
+                            sensorName=sensorName[j],
+                            units=units[j],
+                            timestamp=timestamp,
+                            data=newData
+                        )))
 
         print(f"Conversions Done  | {time.time()-initialTime}")
 
-        #Converting data into a pandas dataframe to be appended to database
+        # Converting data into a pandas dataframe to be appended to database
         df = pd.DataFrame(converted_data)
 
         df['date_time'] = pd.to_datetime(df['timestamp'])
-        #set a DateTime index and delete the old time_stamp columns
+        # set a DateTime index and delete the old time_stamp columns
         df = df.set_index(pd.DatetimeIndex(df['date_time']))
         del df['timestamp'], df['date_time']
-        #Seperate the dataframe by groups of adc'
+        # Seperate the dataframe by groups of adc'
 
         grouped = df.groupby(["sensorName", 'adc', 'channel', 'units'])
-
         print(f"Beginning to write to database | {time.time()-initialTime}")
         for group in grouped.groups:
 
@@ -127,35 +156,38 @@ def main():
             tags = dict(sensorName=sensorName, adc=adc, channel=channel)
 
             sub_df = grouped.get_group(group)[['data']]
-            sub_df.rename(columns={'date_time': 'date_time', 'data': str(units)}, inplace=True)
+            sub_df.rename(
+                columns={'date_time': 'date_time', 'data': str(units)},
+                inplace=True
+            )
 
-            measurementName = location +  "_" + sensorName
+            measurementName = location + "_" + sensorName
 
             influxdb.df_client.write_points(sub_df, measurementName, tags=tags)
 
-            #print(f"Written {measurementName} data to database  | {time.time()-initialTime}")
-            #print(str(sub_df.iloc[[0]]))
         print(f"Written to database | {time.time()-initialTime}")
 
-
-        #Potential Issue. Message still sends even if data fails to get pushed to influx because of try/except statement
-        client.publish(topic_inspector, json.dumps([location, inspectorPayload]))
-        print(f"Published to Inspector  | {time.time()-initialTime}" )
+        # Potential Issue. Message still sends even if data fails to get pushed
+        #  to influx because of try/except statement
+        client.publish(
+            INPECTOR_MQTT_TOPIC,
+            json.dumps([location, inspectorPayload])
+        )
+        print(f"Published to Inspector  | {time.time()-initialTime}")
         print("\n")
-        pass
 
-    #Establish Connection to the MQTT Broker
+    # Establish Connection to the MQTT Broker
+    client, connection = connect_to_broker(
+        client_id=MQTT_CLIENT_ID,
+        host=BROKER_HOST,
+        port=BROKER_PORT,
+        keepalive=BROKER_KEEPALIVE,
+        on_connect=on_connect,
+        on_message=on_message
+    )
 
-    client, connection = connect_to_broker(client_id=client_id, host=HOST, port=PORT, keepalive=KEEPALIVE, on_connect=on_connect, on_message=on_message)
-
-    #Begin the connection loop, where within the loop, messages can be sent
+    # Begin the connection loop, where within the loop, messages can be sent
     client.loop_forever()
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
--- calculator/influxAPI.py
+++ calculator/influxAPI.py
@@ -1,56 +1,99 @@
-from datetime import datetime
-from influxdb import InfluxDBClient, DataFrameClient
+import os
 import time
+from datetime import datetime
+
+from influxdb import InfluxDBClient, DataFrameClient
+from requests.exceptions import ConnectionError
+
+DB_HOST = os.getenv("DB_HOST", "influxdb")
+DB_PORT = int(os.getenv("DB_PORT", "8086"))
+DB_USERNAME = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+DB_NAME = os.getenv("DB_NAME", "sensor_data")
+
 
 class InfluxConnection():
+    """Influx DB API assistant Class."""
 
-	def __init__(self, db_host='10.128.189.236', db_port=8086, db_username='root', db_password='root', database='testing'):
+    def __init__(
+        self,
+        db_host=DB_HOST,
+        db_port=DB_PORT,
+        db_username=DB_USERNAME,
+        db_password=DB_PASSWORD,
+        database=DB_NAME
+    ):
 
-		self.host = db_host
-		self.port = db_port
-		self.username = db_username
-		self.password = db_password
-		self.database = database
+        self.host = db_host
+        self.port = db_port
+        self.username = db_username
+        self.password = db_password
+        self.database = database
 
-		self.db_client = InfluxDBClient(host=self.host, port=self.port, username=self.username, password=self.password, database=self.database)
-		self.wait_for_influxdb()
+        self.db_client = InfluxDBClient(
+            host=self.host,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+            database=self.database
+        )
+        self.wait_for_influxdb()
 
-		self.df_client = DataFrameClient(host=self.host, port=self.port, username=self.username, password=self.password, database=self.database)
-		self.wait_for_dataFrameClient()
+        self.df_client = DataFrameClient(
+            host=self.host,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+            database=self.database
+        )
+        self.wait_for_dataFrameClient()
 
+    def wait_for_influxdb(self):
+        """Function to wait for the influxdb service to be available."""
+        try:
+            self.db_client.ping()
+            print(f"InfluxDBClient Connected | {datetime.now()}")
+            return None
+        except ConnectionError:
+            print("InfluxDBClient Connection FAILED: Trying again (1s)")
+            time.sleep(1)
+            self.wait_for_influxdb()
 
-	def wait_for_influxdb(self):
-		"""Function to wait for the influxdb service to be available"""
-		try:
-			self.db_client.ping()
-			print(f"InfluxDBClient Connected | {datetime.now()}")
-			return None
-		except:
-			print("InfluxDBClient Connection FAILED: Trying again (1s)")
-			time.sleep(1)
-			self.wait_for_influxdb()
+    def wait_for_dataFrameClient(self):
+        """Function to wait for the influxdb service to be available."""
+        try:
+            self.df_client.ping()
+            print(f"DataFrameClient Connected | {datetime.now()} ")
+            return None
+        except ConnectionError:
+            print("DataFrameClient Connection FAILED: Trying again (1s)")
+            time.sleep(1)
+            self.wait_for_dataFrameClient()
 
-	def wait_for_dataFrameClient(self):
-		"""Function to wait for the influxdb service to be available"""
-		try:
-			self.df_client.ping()
-			print(f"DataFrameClient Connected | {datetime.now()} ")
-			return None
-		except:
-			print("DataFrameClient Connection FAILED: Trying again (1s)")
-			time.sleep(1)
-			self.wait_for_dataFrameClient()
+    def get_n_recent_readings(
+        self,
+        number_of_readings,
+        table_name="measurements"
+    ):
+        query_to_execute = (
+            'select * from "{}" group by * order by DESC limit {}'
+        ).format(table_name, number_of_readings)
 
+        return self.db_client.query(query_to_execute).raw['series']
 
-	def get_n_recent_readings(self, number_of_readings, table_name="measurements"):
-		query_to_execute = f'select * from "{table_name}" group by * order by DESC limit {number_of_readings}'
+    def get_n_recent_readings_with_condition(
+        self,
+        number_of_readings,
+        adc,
+        channel,
+        table_name="measurements"
+    ):
+        query_to_execute = (
+            'select * from "{}" where adc=\'{}\' and channel=\'{}\' group by *'
+            ' order by DESC limit {}'
+        ).format(table_name, adc, channel, number_of_readings)
 
-		return self.db_client.query(query_to_execute).raw['series']
+        return self.db_client.query(query_to_execute).raw['series']
 
-	def get_n_recent_readings_with_condition(self, number_of_readings, adc, channel, table_name="measurements"):
-		query_to_execute = f'select * from "{table_name}" where adc=\'{adc}\' and channel=\'{channel}\' group by * order by DESC limit {number_of_readings}'
-
-		return self.db_client.query(query_to_execute).raw['series']
-
-	def push_to_database(self, json_data, table_name="converted_measurements"):
-		pass
+    def push_to_database(self, json_data, table_name="converted_measurements"):
+        pass
